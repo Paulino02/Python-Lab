ef9043668b1ad5f0ea0c34189c5229d4e05132df
Tiago Paulino <tpaulino12000@gmail.com>
2024-03-29 20:07:59 -0300

    att

--- slicing.py
+++ slicing.py
@@ -3,3 +3,8 @@ devops2 = ("linux","aws")
 print(devops[0])
 print(devops2[1])
 print(devops2[0:2])
+
+# dictionary Example
+
+skills = {"devops": ("jenkins","terraform","azure"), "development": ["java", "NodeJS", "kotlin"], "sistemas":["linux", "windows", "macOS"]}
+print(skills)
