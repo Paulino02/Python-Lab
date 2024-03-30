624852571ecf864ae09129db1e7d3dc1e56b9e5d
Tiago Paulino <tpaulino12000@gmail.com>
2024-03-29 22:49:21 -0300

    operadores

---
+++ operadores.py
@@ -0,0 +1,22 @@
+
+#Aritmética básica
+x = 2
+y = 5
+#soma
+total = x + y
+print(total)
+#subitração
+total = x - y
+print(total)
+#multiplicação
+total = x*y
+print(total)
+#divisão
+total = y/x
+print(total)
+
+total = y%x
+print(total)
+#exponenciação
+total = y**x
+print(total)
