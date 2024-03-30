48aa147b21bcb4b65f631f039a17ce480b282cc8
Tiago Paulino <tpaulino12000@gmail.com>
2024-03-29 23:29:52 -0300

    att

---
+++ comparação.py
@@ -0,0 +1,14 @@
+a = 10
+b = 20
+
+out = (a > b)
+print(out)
+
+out = (a < b)
+print(out)
+
+out = (a == b) #igual
+print(out)
+
+out = (a != b) #diferente
+print(out)
---
+++ operadoreslogicos.py
@@ -0,0 +1,14 @@
+a = 20
+b = 90
+
+x = 98
+y = 87
+
+out = (a != b) or (x == y)
+print(out)
+
+out = (a < b) and (x > y)
+print (out)
+
+out = not(x > y)
+print(out)
