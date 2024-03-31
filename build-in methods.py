c765d4842f033e2162085d3438a5fcb01b65f5b5
Tiago Paulino <tpaulino12000@gmail.com>
2024-03-31 18:26:48 -0300

    built in functions

---
+++ build-in methods.py
@@ -0,0 +1,10 @@
+DevOps = ["jenkins", "ansible", "bash", "puppet", "docker", "kubernetes", "terraform"] #lista -> [] 
+Development = ("java", "nodejs", "javascript", ".net", "python") # estrutura de dados -> ()
+temp = {"nome":"tps", "Skill":"AI", "code":1234} # dicionario -> {}
+temp2 ={"nome":"tp", "Skill":"FinOps", "code":12345} #dicionario -> {}
+
+#print(temp.values())
+#DevOps.pop()
+#print(DevOps)
+
+print("/".join(Development))
